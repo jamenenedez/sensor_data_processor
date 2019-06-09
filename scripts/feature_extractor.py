9d5720946e77d1e68ca76031c1849e0cfd91af75
Jose Armando Menendez Clavijo <armandom@advancio.com>
2019-06-09 01:32:16 -0700

    Adding descriptor extractor script

--- scripts/feature_extractor.py
+++ scripts/feature_extractor.py
@@ -1,34 +1,113 @@
 # Jose Menendez
 # Feature statistics
 
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
 def zero_crossing_rate(data):
     import numpy as np
     my_array = np.array(data)
     return ((my_array[:-1] * my_array[1:]) < 0).sum()
 
+
+def standar_deviation(data):
+    import statistics
+    return statistics.stdev(data)
+
+
+def qmean(data):
+    import math
+    return math.sqrt(sum(n*n for n in data)/len(data))
+
+
+def arithmetic_mean(data):
+    import statistics
+    return statistics.mean(data)
+
+
+def magnitude(x, y, z):
+    import math
+    return round(math.sqrt(pow(float(x), 2) + pow(float(y), 2) + pow(float(z), 2)), 3)
+
+
 def read():
     import os
     import sys
-    import math
-    from os import listdir
+
     arguments = sys.argv
-    dir = "."   
+    dir = "."
 
     if not arguments[1]:
         sys.exit('Error in path')
     else:
         dir = arguments[1]
 
-    for f in listdir(dir):        
-        w_ext = f.split(".")
-        if len(w_ext) > 1 and w_ext[1] in ['txt', 'csv']:
-             # Open & creation of files
-            file = open(os.path.join(dir, f), 'r')
-            line = file.readline()
-            
-            while line:
-                line = file.readline()                
-                words = line.split(",")
-                
-               #magnitude = round(math.sqrt(pow(float(words[1]), 2) + pow(float(words[2]), 2) + pow(float(words[3]), 2)), 3)                
+    directories = []
+
+    resultDir = os.getcwd()+"/../results"
+    if not os.path.isdir(resultDir):
+        os.mkdir(resultDir)
+
+    descriptor = open(resultDir+'/descriptor.csv', 'w')
+    descriptor.write(
+        "arithmetic_average,standard_deviation,mean_squares,class\n")
+
+    for r, d, f in os.walk(dir):
+        directories.append(r)
+
+    for d in directories:
+        tokens = d.split("\\")
+        if len(tokens) == 4:
+            sensor = tokens[2]
+            action = tokens[3]
+            if sensor == "movil":
+                movil_dir = os.path.join(dir, str(sensor), str(action))
+                for m_f in os.listdir(movil_dir):
+                    data = []
+                    file = open(os.path.join(movil_dir, m_f), 'r')
+                    line = file.readline()
+
+                    while line != "":
+                        line = file.readline()
+                        values = line.split(",")
+                        if 4 in range(-len(values), len(values)) and is_number(values[4]):
+                            data.append(float(values[4]))
+                    
+                    row = [str(arithmetic_mean(data)), str(standar_deviation(data)), str(qmean(data)), str(action)]        
+                    descriptor.write(",".join(row)+"\n")
+            elif sensor == "reloj":
+                smart_watch_dir = os.path.join(dir, str(sensor), str(action))
+                for sw_f in os.listdir(smart_watch_dir):
+                    data = []
+                    file = open(os.path.join(smart_watch_dir, sw_f), 'r')
+                    line = file.readline()
+
+                    while line != "":
+                        line = file.readline()
+                        values = line.split(",")
+                        if 3 in range(-len(values), len(values)) and is_number(values[3]):
+                            data.append(float(values[3]))
+                    
+                    row = [str(arithmetic_mean(data)), str(standar_deviation(data)), str(qmean(data)), str(action)]        
+                    descriptor.write(",".join(row)+"\n")
+            elif sensor == "sensor_1":
+                inertial_sensor_dir = os.path.join(dir, str(sensor), str(action))
+                for is_f in os.listdir(inertial_sensor_dir):
+                    data = []
+                    file = open(os.path.join(inertial_sensor_dir, is_f), 'r')
+                    line = file.readline()
+
+                    while line != "":
+                        line = file.readline()
+                        values = line.split(",")
+                        if 5 in range(-len(values), len(values)) and is_number(values[3]) and is_number(values[4]) and is_number(values[5]):
+                            data.append(float(magnitude(values[3], values[4], values[5])))
+                    
+                    row = [str(arithmetic_mean(data)), str(standar_deviation(data)), str(qmean(data)), str(action)]        
+                    descriptor.write(",".join(row)+"\n")
+
 read()
